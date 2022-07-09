a47db615b69ca2cbfbb5ad62d790d9387ed47741
Armaan Gupta <anthracene9@protonmail.com>
2022-07-09 16:55:16 +0530

    Added navbar

--- pages/News/index.js
+++ pages/News/index.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
-import Image from 'next/image';
+import Link from "next/link";
 
 export default function News({articles}) {
   return (
+	  <>
+	  <div className='w-full mt-4 flex items-center justify-around'>
+		<Link href="/">
+			<a className='text-blue-900 underline font-bold uppercase'>Go to /</a>
+		</Link>
+		<h1 className='uppercase font-bold text-2xl pl-4 pt-4 underline'>Armaan's news website 🚀</h1>
+		
+	  </div>
     <div className="w-screen flex flex-column items-center justify-center min-h-screen p-6">
       <div className="w-[80%]">
         {articles.map(article => {
@@ -13,7 +21,6 @@ export default function News({articles}) {
                   <h1 className="uppercase font-normal">{article.title}</h1>
                   <p className="mt-2 text-gray-500">{article.description}</p>
                 </div>
-
                 <img src={article.urlToImage} className="w-[300px] h-[180px]" />
               </div>
               <hr />
@@ -22,6 +29,7 @@ export default function News({articles}) {
         })}
       </div>
     </div>
+	  </>
   );
 }
 
@@ -30,7 +38,6 @@ export async function getServerSideProps() {
     const news = await axios.get(
       'https://saurav.tech/NewsAPI/everything/cnn.json',
     );
-    console.log(news.data.articles);
     return {props: {articles: news.data.articles}};
   } catch (e) {
     console.log(e.message);
